f67f26c1fbf30d9a6c58920877bebcf61eed3e3a
ridhoaulia <muhammadridhoaulia@gmail.com>
2014-12-23 15:33:35 +0700

    commit kedua

---
+++ src/com/ridho/tekatekigambar/DonaturresmiActivity.java
@@ -0,0 +1,58 @@
+package com.ridho.tekatekigambar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class DonaturresmiActivity extends Activity {
+	EditText jawabanTxt;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_donaturresmi);
+		//Cari komponen edit text
+        jawabanTxt = (EditText) findViewById(R.id.editText1);
+       //Setting input dalam huruf kapital
+        jawabanTxt.setFilters(new InputFilter[] {new InputFilter.AllCaps()});
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.donaturresmi, menu);
+		return true;
+	}
+	public void cekJawaban(View v){
+    	String jawabanBenar = "DONATUR RESMI";
+    	String jawabanUser = jawabanTxt.getText().toString();
+    	if(jawabanUser.equals(jawabanBenar)){
+    		Toast.makeText(getApplicationContext(), 
+    				"JAWABAN TEPAT !", Toast.LENGTH_LONG).show();
+    		Intent i = new Intent(getApplicationContext(),JejakatakutActivity.class);
+    		startActivity(i);
+    	}
+    	else{
+    		//Pesan Text
+    		Toast.makeText(getApplicationContext(),
+    				"JAWABAN MASIH SALAH !", Toast.LENGTH_LONG).show();
+    	}
+    	
+    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
